1023eaed2cc94f8b57e7ce48e4b5eb2b074eff85
StijnVM <stijn.vm@hotmail.com>
2025-05-13 14:02:35 +0200

    homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ArticleComponent } from './components/ui/article/article.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'subject/:subjectName', component: ArticleComponent },
+  { path: '**', redirectTo: '' },
 ];
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RssService } from '../../../services/rss.service';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from '../../ui/article/article.component';
-import { Article } from '../../../types/types';
+import { Article, SubjectGroup } from '../../../types/types';
+import { SUBJECT_GROUPS } from '../../../types/vars';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +16,14 @@ export class HomeComponent implements OnInit {
   constructor(private rss: RssService) {}
 
   articles: Article[] = [];
+  subjectGroups: SubjectGroup[] = SUBJECT_GROUPS;
 
   async ngOnInit(): Promise<void> {
     this.articles = await this.rss.getRssAsync();
+    console.log('groups', this.subjectGroups);
+  }
+
+  getLink(subject: string): string {
+    return `${encodeURIComponent(subject)}`;
   }
 }
